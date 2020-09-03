bbab19572cb2e62ec36ed831458272e23a970bf8
davidsaemund <davidsae14@ru.is>
2020-09-03 13:49:45 +0000

    Bæta við comments

--- max_int.py
+++ max_int.py
@@ -1,10 +1,12 @@
 
 num_int = int(input("Input a number: "))    # Do not change this line
-# Fill in the missing code
+
 max_int = 0
+
 # While num_int er positive number ask again
 
 while num_int >= 0 :
+    # If max_int is less than num_int, set max_int == num_int
     if num_int >= max_int :
         max_int = num_int
     num_int = int(input("Input a number: "))
